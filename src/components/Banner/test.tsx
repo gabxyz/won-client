b9aab639543fe8a00d85abea45d27c4088cad3c4
Gabriel <gabxyzdev@gmail.com>
2022-01-08 14:19:45 -0300

    Implement the Ribbon component into the Banner component
    Add types for the ribbon in the index.tsx file using the previous created types for the ribbon properties and use conditional rendering for the ribbon render
    Add a storybook component in the stories.tsx file for the Banner with the Ribbon and pass some args and default styling
    Add media query in the styles.ts file to adapt Ribbon styles for mobile screens
    Add test cases for rendering the Banner component with the Ribbon component in the test.tsx file

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -4,14 +4,14 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import Banner from '.'
 
 describe('<Banner />', () => {
+  const props = {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
   it('should render correctly', () => {
-    const props = {
-      img: 'https://source.unsplash.com/user/willianjusten/1042x580',
-      title: 'Defy death',
-      subtitle: '<p>Play the new <strong>CrashLands</strong> season',
-      buttonLabel: 'Buy now',
-      buttonLink: '/games/defy-death'
-    }
     const { container } = renderWithTheme(<Banner {...props} />)
 
     expect(
@@ -25,5 +25,24 @@ describe('<Banner />', () => {
     expect(screen.getByRole('img', { name: /defy death/i })).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render a Ribbon', () => {
+    renderWithTheme(
+      <Banner
+        {...props}
+        ribbon="My Ribbon"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />
+    )
+    const ribbon = screen.getByText(/My Ribbon/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({ backgroundColor: '#3CD3C1' })
+    expect(ribbon).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
+    })
   })
 })
