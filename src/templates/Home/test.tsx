151572d19e52aa5b219a8b4fed3f2b3bbca278ee
Gabriel <gabxyzdev@gmail.com>
2022-04-12 09:18:13 -0300

    Add styles for the different sections of the home page, mainly changes in the spacings, also add new color to the theme for a bg section style

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -34,10 +34,6 @@ describe('<Home />', () => {
   it('should render the sections', () => {
     renderWithTheme(<Home {...props} />)
 
-    expect(screen.getByRole('heading', { name: /news/i })).toHaveStyleRule(
-      'color',
-      '#030517'
-    )
     expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
     expect(
       screen.getByRole('heading', { name: /most popular/i })
