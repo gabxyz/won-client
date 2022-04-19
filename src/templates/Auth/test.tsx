2907c684f84fba3472e8b580d70c26311236a665
Gabriel <gabxyzdev@gmail.com>
2022-04-19 16:52:20 -0300

    Add the Auth template component for auth pages index.tsx adds the initial structure for the pages styles.ts adds the styles structure test.tsx implements tests for the correct rendering of the components and the children

---
+++ src/templates/Auth/test.tsx
@@ -0,0 +1,29 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Auth from '.'
+
+describe('<Auth />', () => {
+  it('should render all components and children', () => {
+    renderWithTheme(
+      <Auth title="Auth title">
+        <input type="text" />
+      </Auth>
+    )
+
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+    expect(
+      screen.getByRole('heading', { name: /All your favorite/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /won is the best/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /auth title/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
+  })
+})
