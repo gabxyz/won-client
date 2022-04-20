c19c52bee0225def305b84ad86c7e9a3008f3380
Gabriel <gabxyzdev@gmail.com>
2022-04-20 10:03:05 -0300

    Fix global font styles to use correct local names

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,31 +14,29 @@ const GlobalStyles: GlobalStyleComponent<
   DefaultTheme
 > = createGlobalStyle`
 
-  @font-face {
+@font-face {
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 300;
     font-display: swap;
-    src: local(''),
-        url('/fonts/poppins-v15-latin-300.woff2') format('woff2'),
+    src: local('Poppins Light'), local('Poppins-Light'),
+        url('/fonts/poppins-v15-latin-300.woff2') format('woff2');
   }
-
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 400;
     font-display: swap;
-    src: local(''),
-        url('/fonts/poppins-v15-latin-regular.woff2') format('woff2'),
+    src: local('Poppins Regular'), local('Poppins-Regular'),
+        url('/fonts/poppins-v15-latin-regular.woff2') format('woff2');
   }
-
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 600;
     font-display: swap;
-    src: local(''),
-        url('/fonts/poppins-v15-latin-600.woff2') format('woff2'),
+    src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
+        url('/fonts/poppins-v15-latin-600.woff2') format('woff2');
   }
 
   * {
