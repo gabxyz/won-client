559f05e23af58d3a4c28a525c244c494dbfa6116
Gabriel <gabxyzdev@gmail.com>
2022-04-23 15:44:25 -0300

    Add component structure, styles and tests for the FormSignUp component index.tsx adds form components for the sign up styles.ts adds styles for the wrapper and the form link and text test.tsx adds tests for the form elements and text rendering

--- src/components/FormSignUp/index.tsx
+++ src/components/FormSignUp/index.tsx
@@ -1,8 +1,48 @@
+import { AccountCircle, Email, Lock } from '@styled-icons/material-outlined'
+import Link from 'next/link'
+
+import TextField from 'components/TextField'
+import Button from 'components/Button'
+
 import * as S from './styles'
 
 const FormSignUp = () => (
   <S.Wrapper>
-    <h1>FormSignUp</h1>
+    <form>
+      <TextField
+        name="name"
+        placeholder="Name"
+        type="text"
+        icon={<AccountCircle />}
+      />
+      <TextField
+        name="email"
+        placeholder="Email"
+        type="email"
+        icon={<Email />}
+      />
+      <TextField
+        name="password"
+        placeholder="Password"
+        type="password"
+        icon={<Lock />}
+      />
+      <TextField
+        name="password"
+        placeholder="Confirm password"
+        type="password"
+        icon={<Lock />}
+      />
+      <Button size="large" fullWidth>
+        Sign up now
+      </Button>
+      <S.FormLink>
+        Already have an account?{' '}
+        <Link href="sign-in">
+          <a>Sign in</a>
+        </Link>
+      </S.FormLink>
+    </form>
   </S.Wrapper>
 )
 
