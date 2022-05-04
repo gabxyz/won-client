fd93d018522307f109dfe60cfbe66c5b1f5369aa
Gabriel <gabxyzdev@gmail.com>
2022-05-04 15:59:36 -0300

    Add a Close button to the modal along with tests and styles index.tsx adds a close button and uses the state hook to handle logic styles.ts adds the modal styles and the close button styles, close button acts as the overlay to close the modal test.tsx implements the tests to handle the modal closing correctly

--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -33,4 +33,20 @@ describe('<Gallery />', () => {
     expect(modal.getAttribute('aria-hidden')).toBe('false')
     expect(modal).toHaveStyle({ opacity: 1 })
   })
+
+  it('should handle close modal when overlay or button is clicked', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    const modal = screen.getByLabelText('modal')
+
+    fireEvent.click(
+      screen.getByRole('button', { name: /thumb - gallery image 1/i })
+    )
+
+    //click to close modal
+    fireEvent.click(screen.getByRole('button', { name: /close modal/i }))
+
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0 })
+  })
 })
