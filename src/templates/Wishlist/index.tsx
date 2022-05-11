81abcb2d134fff9c70a19b3c2ff51bedd3d8cd05
Gabriel <gabxyzdev@gmail.com>
2022-05-11 16:05:11 -0300

    Add the Divider component below the GameCards for better design and remove unused styles.ts file

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -6,6 +6,7 @@ import GameCard, { GameCardProps } from 'components/GameCard'
 import { Container } from 'components/Container'
 import { Grid } from 'components/Grid'
 import { HighlightProps } from 'components/Highlight'
+import { Divider } from 'components/Divider'
 
 export type WishlistTemplateProps = {
   games?: GameCardProps[]
@@ -29,6 +30,8 @@ const Wishlist = ({
           <GameCard key={`wishlist-${index}`} {...game} />
         ))}
       </Grid>
+
+      <Divider />
     </Container>
 
     <Showcase
