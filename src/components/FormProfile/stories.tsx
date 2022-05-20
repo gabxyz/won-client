f603d77317083867e5a53f63991afae95b3e6301
Gabriel <gabxyzdev@gmail.com>
2022-05-20 16:43:14 -0300

    Add the Form component responsive styles, removes the Wrapper component and adds a styled div to center the component in the storybook page

--- src/components/FormProfile/stories.tsx
+++ src/components/FormProfile/stories.tsx
@@ -2,8 +2,12 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 import FormProfile from '.'
 
 export default {
-  title: 'FormProfile',
+  title: 'Form/FormProfile',
   component: FormProfile
 } as Meta
 
-export const Default: Story = (args) => <FormProfile {...args} />
+export const Default: Story = (args) => (
+  <div style={{ maxWidth: 860, margin: 'auto' }}>
+    <FormProfile {...args} />
+  </div>
+)
