47788d8bca19cf05b5140776911be90e199d89d8
Gabriel <gabxyzdev@gmail.com>
2022-06-22 16:37:33 -0300

    Refactor Home template to use only one Showcase component for the Upcoming section

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -15,7 +15,6 @@ const props: HomeTemplateProps = {
   mostPopularGames: [gamesMock[0]],
   upcomingGames: [gamesMock[0]],
   upcomingHighlight: highlightMock,
-  upcomingMoreGames: [gamesMock[0]],
   freeGames: [gamesMock[0]],
   freeHighlight: highlightMock
 }
@@ -42,6 +41,6 @@ describe('<Home />', () => {
     renderWithTheme(<Home {...props} />)
 
     expect(screen.getByTestId('Mock Banner Slider')).toBeInTheDocument()
-    expect(screen.getAllByTestId('Mock Showcase')).toHaveLength(5)
+    expect(screen.getAllByTestId('Mock Showcase')).toHaveLength(4)
   })
 })
