7492c9640c92c2d915c0c3199b8186c11c5daf20
Gabriel <gabxyzdev@gmail.com>
2022-06-27 14:52:21 -0300

    Refactor games page to fetch data in the client instead of SSR
    - games.tsx sets initialApolloState prop to use the query cache data in the client
    - index.tsx fetch data with useQuery hook method to render the game data in the client, also add handleShowMore function logic using fetchMore function from apollo to handle pagination
    
    The limit variable value for the queries is incresead from 9 to 15

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -1,3 +1,5 @@
+import { useQueryGames } from 'graphql/queries/games'
+
 import Base from 'templates/Base'
 import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined/KeyboardArrowDown'
 
@@ -6,38 +8,55 @@ import GameCard, { GameCardProps } from 'components/GameCard'
 import { Grid } from 'components/Grid'
 
 import * as S from './styles'
+import GamesLoader from 'components/GamesLoader'
 
 export type GamesTemplateProps = {
   filterItems: ItemProps[]
   games?: GameCardProps[]
 }
 
-const GamesTemplate = ({ games = [], filterItems }: GamesTemplateProps) => {
+const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
+  const { data, loading, fetchMore } = useQueryGames({
+    variables: { limit: 15 }
+  })
+
   const handleFilter = () => {
     return
   }
 
   const handleShowMore = () => {
-    return
+    fetchMore({ variables: { limit: 15, start: data?.games.length } })
   }
 
   return (
     <Base>
       <S.Main>
         <ExploreSidebar items={filterItems} onFilter={handleFilter} />
+        {loading ? (
+          <S.GamesLoader>
+            <GamesLoader />
+          </S.GamesLoader>
+        ) : (
+          <section>
+            <Grid>
+              {data?.games.map((game) => (
+                <GameCard
+                  key={game.slug}
+                  title={game.name}
+                  slug={game.slug}
+                  developer={game.developers[0].name}
+                  img={`http://localhost:1337${game.cover!.url}`}
+                  price={game.price}
+                />
+              ))}
+            </Grid>
 
-        <section>
-          <Grid>
-            {games.map((game) => (
-              <GameCard key={game.img} {...game} />
-            ))}
-          </Grid>
-
-          <S.ShowMore role="button" onClick={handleShowMore}>
-            <p>Show More</p>
-            <ArrowDown size={35} />
-          </S.ShowMore>
-        </section>
+            <S.ShowMore role="button" onClick={handleShowMore}>
+              <p>Show More</p>
+              <ArrowDown size={35} />
+            </S.ShowMore>
+          </section>
+        )}
       </S.Main>
     </Base>
   )
