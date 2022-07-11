9eca656514ce4467272be04598d893b72c4087e6
Gabriel <gabxyzdev@gmail.com>
2022-07-11 16:34:23 -0300

    Refactor to handle 0 and null price values

--- src/utils/format-price.ts
+++ src/utils/format-price.ts
@@ -1,4 +1,10 @@
 export default function formatPrice(price: number | bigint): string {
+  if (price === 0) {
+    return 'FREE'
+  }
+  if (price === null) {
+    return 'Coming Soon'
+  }
   return new Intl.NumberFormat('en', {
     style: 'currency',
     currency: 'USD'
