6682459983f8cbe0ec808dd1f5fac87ee15b6d30
Gabriel <gabxyzdev@gmail.com>
2022-07-12 15:24:42 -0300

    Refactor limit variable to be optional and generate new query types

--- src/graphql/generated/QueryGames.ts
+++ src/graphql/generated/QueryGames.ts
@@ -44,7 +44,7 @@ export interface QueryGames {
 }
 
 export interface QueryGamesVariables {
-  limit: number;
+  limit?: number | null;
   start?: number | null;
   where?: any | null;
   sort?: string | null;
