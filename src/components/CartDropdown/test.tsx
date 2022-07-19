d8eea18b346c879beb8bca715d63377d9c353d87
Gabriel <gabxyzdev@gmail.com>
2022-07-19 16:15:27 -0300

    Refactor to get quantity from the cartProviderProps

--- src/components/CartDropdown/test.tsx
+++ src/components/CartDropdown/test.tsx
@@ -1,3 +1,4 @@
+import { CartContextDefaultValues } from 'hooks/use-cart'
 import { render, screen } from 'utils/test-utils'
 
 import items from 'components/CartList/mock'
@@ -6,10 +7,12 @@ import CartDropdown from '.'
 
 describe('<CartDropdown />', () => {
   it('should render CartIcon and its badge', () => {
-    render(<CartDropdown items={items} total="$65.00" />)
+    render(<CartDropdown items={items} total="$65.00" />, {
+      cartProviderProps: { ...CartContextDefaultValues, quantity: 3 }
+    })
 
     expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
-    expect(screen.getByText(`${items.length}`)).toBeInTheDocument()
+    expect(screen.getByText(3)).toBeInTheDocument()
   })
 
   it('should render Dropdown content with cart items and total', () => {
