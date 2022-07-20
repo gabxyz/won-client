67b23aa9fb00e0ceff9ecdd3b18387ebc2dc886d
Gabriel <gabxyzdev@gmail.com>
2022-07-20 17:15:01 -0300

    Add new id prop to gameInfo fields

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -75,6 +75,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     props: {
       cover: `http://localhost:1337${game.cover?.src}`,
       gameInfo: {
+        id: game.id,
         title: game.name,
         basePrice: game.basePrice,
         price: game.price,
