56926a2a0df526ac6979923bb572da28231596dd
Gabriel <gabxyzdev@gmail.com>
2022-08-09 15:36:37 -0300

    Add use-wishlist custom hook with mock and tests
    - index.tsx adds initial hook structure with context types, context default values and a WishlistProvider that handles session logic to query wishlist data and methods to handle wishlist state.
    - mock.ts adds wishlistMock with graphql structure and wishlistItems with dummy data.
    - test.tsx adds tests to check for wishlist items and correct behavior of isInWishlist method.

---
+++ src/hooks/use-wishlist/index.tsx
@@ -0,0 +1,77 @@
+import { GameCardProps } from 'components/GameCard'
+import { QueryWishlist_wishlists_games } from 'graphql/generated/QueryWishlist'
+import { useQueryWishlist } from 'graphql/queries/wishlist'
+import { useSession } from 'next-auth/client'
+import { createContext, useContext, useEffect, useState } from 'react'
+import { gamesMapper } from 'utils/mappers'
+
+export type WishlistContextData = {
+  items: GameCardProps[]
+  isInWishlist: (id: string) => boolean
+  addToWishlist: (id: string) => void
+  removeFromWishlist: (id: string) => void
+  loading: boolean
+}
+
+export const WishlistContextDefaultValues = {
+  items: [],
+  isInWishlist: () => false,
+  addToWishlist: () => null,
+  removeFromWishlist: () => null,
+  loading: false
+}
+
+export const WishlistContext = createContext<WishlistContextData>(
+  WishlistContextDefaultValues
+)
+
+export type WishlistProviderProps = {
+  children: React.ReactNode
+}
+
+const WishlistProvider = ({ children }: WishlistProviderProps) => {
+  const [session] = useSession()
+  const [wishlistItems, setWishlistItems] = useState<
+    QueryWishlist_wishlists_games[]
+  >([])
+
+  const { data, loading } = useQueryWishlist({
+    skip: !session?.user?.email,
+    context: { session },
+    variables: {
+      identifier: session?.user?.email as string
+    }
+  })
+
+  useEffect(() => {
+    setWishlistItems(data?.wishlists[0]?.games || [])
+  }, [data])
+
+  const isInWishlist = (id: string) =>
+    !!wishlistItems.find((game) => game.id === id)
+
+  const addToWishlist = (id: string) => {
+    id
+  }
+  const removeFromWishlist = (id: string) => {
+    id
+  }
+
+  return (
+    <WishlistContext.Provider
+      value={{
+        items: gamesMapper(wishlistItems),
+        isInWishlist,
+        addToWishlist,
+        removeFromWishlist,
+        loading
+      }}
+    >
+      {children}
+    </WishlistContext.Provider>
+  )
+}
+
+const useWishlist = () => useContext(WishlistContext)
+
+export { WishlistProvider, useWishlist }
