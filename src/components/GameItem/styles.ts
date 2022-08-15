4036063b9f40b68e8289034d2570c95d5d67fd7c
Gabriel <gabxyzdev@gmail.com>
2022-08-15 14:40:19 -0300

    Refactor styles for better responsiveness and design

--- src/components/GameItem/styles.ts
+++ src/components/GameItem/styles.ts
@@ -5,7 +5,7 @@ import { darken } from 'polished'
 
 export const Wrapper = styled.div`
   ${({ theme }) => css`
-    padding: ${theme.spacings.small};
+    padding: ${theme.spacings.xsmall};
     border-bottom: 0.1rem solid ${theme.colors.lightGray};
 
     ${media.greaterThan('medium')`
@@ -23,19 +23,20 @@ export const ImageBox = styled.div`
   ${({ theme }) => css`
     flex-shrink: 0;
     margin-right: 1.2rem;
-    width: 9.6rem;
-    height: 5.6rem;
+    width: 12rem;
+    height: 7rem;
 
     img {
       width: 100%;
       height: 100%;
       object-fit: cover;
+      border-radius: ${theme.border.radius};
     }
 
     ${media.greaterThan('medium')`
       margin-right: ${theme.spacings.xsmall};
-      width: 15rem;
-      height: 7rem;
+      width: 18rem;
+      height: 8.6rem;
     `}
   `}
 `
@@ -43,7 +44,12 @@ export const ImageBox = styled.div`
 export const PriceBox = styled(BuyBox)`
   ${({ theme }) => css`
     flex-direction: row-reverse;
-    gap: ${theme.spacings.xxsmall};
+    gap: 0.5rem;
+    font-size: ${theme.font.sizes.small};
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.medium};
+    `}
   `}
 `
 
@@ -58,8 +64,12 @@ export const Remove = styled.div`
   ${({ theme }) => css`
     cursor: pointer;
     color: ${theme.colors.primary};
-    font-size: ${theme.font.sizes.small};
+    font-size: ${theme.font.sizes.xsmall};
     margin-top: ${theme.spacings.xxsmall};
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.small};
+    `}
 
     &:hover {
       color: ${darken(0.2, theme.colors.primary)};
@@ -71,19 +81,20 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  justify-content: space-between;
   width: 100%;
 `
 
 export const Title = styled.h3`
   ${({ theme }) => css`
     font-size: ${theme.font.sizes.small};
-    line-height: ${theme.font.sizes.small};
+    line-height: ${theme.font.sizes.xsmall};
     font-weight: ${theme.font.bold};
     color: ${theme.colors.black};
     margin-bottom: ${theme.spacings.xxsmall};
 
     ${media.greaterThan('medium')`
-      font-size: 2rem;
+      font-size: ${theme.font.sizes.medium};
       line-height: 2rem;
     `}
   `}
